4ea654d3030bb5fea85cc02f8210056770cbf1e1
thecreatorzx <saadyesican@gmail.com>
2025-06-22 16:29:35 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://job-application-tracker-lake.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI, {
